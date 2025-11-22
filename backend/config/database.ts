ab68bd5914ef50ac9206b53cfdfba9fabb01fe6c
Barrie Freyre <barriedirk@yahoo.com>
2025-11-21 22:48:10 -0500

    add strappi for backend, next for frontend

---
+++ backend/config/database.ts
@@ -0,0 +1,42 @@
+import path from "path";
+
+export default ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const connections = {
+    postgres: {
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "kanban_db"),
+        user: env("DATABASE_USERNAME", "kanban_user"),
+        password: env("DATABASE_PASSWORD", "mysecretpassword"),
+        schema: env("DATABASE_SCHEMA", "public"),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
---
+++ commands.md
@@ -0,0 +1,30 @@
+npm create next-app frontend --typescript
+
+pnpm add zustand
+
+ipnpm add @tanstack/react-query
+
+pnpm add -D @types/react
+
+pnpm add @tanstack/react-query-devtools
+
+
+pnpm dev	Levanta la app en modo desarrollo
+pnpm build	Compila la app para producción
+pnpm start	Corre la versión compilada (después de build)
+pnpm lint	Ejecuta ESLint
+pnpm format	Formatea el código (si tienes prettier configurado)
+
+
+
+backend
+-----------
+
+pnpm create strapi-app backend --quickstart #sqlite
+
+pnpm rebuild better-sqlite3
+pnpm add pg
+pnpm run develo
+
+http://localhost:1337/adminp
+
