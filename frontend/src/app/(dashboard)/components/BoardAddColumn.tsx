1b32d4c63b2268d54680a50fc48ca136f4d28150
Barrie Freyre <barriedirk@yahoo.com>
2026-03-31 21:47:15 -0500

    add action for new column

--- frontend/src/app/(dashboard)/components/BoardAddColumn.tsx
+++ frontend/src/app/(dashboard)/components/BoardAddColumn.tsx
@@ -1,15 +1,33 @@
 "use client";
 
+import EditBoardFeature from "@/features/board/components/EditBoardFeature";
+import { useBoardMenuActions } from "../hooks/useBoardHeaderActions";
 import "./board-add-column.css";
 
 export default function BoardAddColumn() {
+  const { activeBoard, activeModal, closeModal, openModal } =
+    useBoardMenuActions();
+
   return (
-    <section className="board-column__add-new  h-full px-4">
-      <div className="bg-background-tertiary flex justify-center items-center h-full mt-13.75">
-        <button className="text-heading-xl text-medium-grey">
-          + New Column
-        </button>
-      </div>
-    </section>
+    <>
+      {activeModal === "edit" && activeBoard && (
+        <EditBoardFeature
+          board={activeBoard}
+          open={true}
+          onClose={closeModal}
+        />
+      )}
+
+      <section className="board-column__add-new  h-full px-4">
+        <div className="bg-background-tertiary flex justify-center items-center h-full mt-13.75">
+          <button
+            className="text-heading-xl text-medium-grey"
+            onClick={() => openModal("edit")}
+          >
+            + New Column
+          </button>
+        </div>
+      </section>
+    </>
   );
 }
